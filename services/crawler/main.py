4be36f0edcedddb8aa9f0111d61fc28ea58ed958
lexuancuong <cuong.le@parcelperform.com>
2023-04-23 23:00:30 +0700

    :yum: Simplify Crawler settings

--- services/crawler/main.py
+++ services/crawler/main.py
@@ -5,11 +5,11 @@ from scrapy import cmdline
 
 
 def crawl_aqi():
-    cmdline.execute('scrapy runspider /srv/crawler/aqi/spiders/iqair.py'.split())
+    cmdline.execute('scrapy runspider /srv/crawler/spiders/iqair.py'.split())
 
 
 if __name__ == '__main__':
-    schedule.every().hour.at(":02").do(crawl_aqi)
+    schedule.every().minute.at(":02").do(crawl_aqi)
     print(schedule.get_jobs())
     while True:
         schedule.run_pending()
---
+++ services/crawler/settings.py
@@ -0,0 +1,11 @@
+BOT_NAME = 'aqi'
+
+SPIDER_MODULES = ['spiders']
+NEWSPIDER_MODULE = 'spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'quotesbot (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
